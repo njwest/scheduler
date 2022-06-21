28adee3171f3d016273391442dc6e9963d2abe01
njwest <njwest9@gmail.com>
2022-06-21 10:07:04 -0500

    doc + rename find range overlaps/3 func

--- lib/scheduler.ex
+++ lib/scheduler.ex
@@ -1,5 +1,20 @@
 defmodule Scheduler do
-  def find_range_overlap(list_one, list_two, duration \\ 1) do
+
+  @doc """
+  Range overlap finder, finds overlaps between two given lists.
+  Default duration is 1.
+
+  ## Examples
+
+      iex> Scheduler.find_range_overlaps([[0, 15], [30, 60], [80, 85]], [[10, 20], [40, 55], [58, 70], [100, 120]], 5)
+
+      [[10, 15], [40, 45]]
+
+      iex> Scheduler.find_range_overlaps([[20, 60], [150, 200]], [[58, 80], [100, 125]], 5)
+
+      []
+  """
+  def find_range_overlaps(list_one, list_two, duration \\ 1) do
     Enum.reduce(list_one, [], fn [start_one, end_one], acc ->
       acc ++
         Enum.reduce(list_two, [], fn [start_two, end_two], acc ->
--- test/scheduler_test.exs
+++ test/scheduler_test.exs
@@ -3,7 +3,7 @@ defmodule SchedulerTest do
   doctest Scheduler
 
   test "find range overlap" do
-    assert Scheduler.find_range_overlap([[0, 15], [30, 60], [80, 85]], [[10, 20], [40, 55], [58, 70], [100, 120]], 5) == [[10, 15], [40, 45]]
+    assert Scheduler.find_range_overlaps([[0, 15], [30, 60], [80, 85]], [[10, 20], [40, 55], [58, 70], [100, 120]], 5) == [[10, 15], [40, 45]]
   end
 
   test "get first overlap" do
