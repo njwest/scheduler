550e1874d6f9fd7a1be365c2586382b1fd412a46
njwest <njwest9@gmail.com>
2022-06-21 10:14:27 -0500

    get first overlap function + docs

--- lib/scheduler.ex
+++ lib/scheduler.ex
@@ -1,4 +1,34 @@
 defmodule Scheduler do
+  @moduledoc """
+  Documentation for `Scheduler`.
+  """
+
+  @doc """
+  Get first overlap between two lists of a given duration (default duration = 1).
+
+  Returns `nil` if no overlap of a given duration is found between the given lists.
+
+  ## Examples
+
+      iex> Scheduler.get_first_overlap([[0, 15], [30, 60], [80, 85]], [[10, 20], [40, 55], [58, 70], [100, 120]], 5)
+
+      [10, 15]
+
+      iex> Scheduler.get_first_overlap([[20, 60], [150, 200]], [[58, 80], [100, 125]], 5)
+
+      nil
+  """
+  def get_first_overlap(list_one, list_two, duration \\ 1) do
+    case find_range_overlaps(list_one, list_two, duration) do
+      # NOTE Pattern matching a list with [head | tail] will always work for a non-empty list,
+      # even there is no tail, e.g.:
+        # if [head | _] = [1], then head == 1
+      [first_overlap | _] ->
+        first_overlap
+      _ ->
+        nil
+    end
+  end
 
   @doc """
   Range overlap finder, finds overlaps between two given lists.
