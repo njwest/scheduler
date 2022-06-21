5ccd57d47175bf0529bea322aff7e38df14aa730
njwest <njwest9@gmail.com>
2022-06-21 10:14:37 -0500

    get first overlap tests

--- test/scheduler_test.exs
+++ test/scheduler_test.exs
@@ -2,11 +2,15 @@ defmodule SchedulerTest do
   use ExUnit.Case
   doctest Scheduler
 
-  test "find range overlap" do
+  test "find range overlaps" do
     assert Scheduler.find_range_overlaps([[0, 15], [30, 60], [80, 85]], [[10, 20], [40, 55], [58, 70], [100, 120]], 5) == [[10, 15], [40, 45]]
   end
 
   test "get first overlap" do
-    assert false
+    assert Scheduler.get_first_overlap([[0, 15], [30, 60], [80, 85]], [[10, 20], [40, 55], [58, 70], [100, 120]], 5) == [10, 15]
+  end
+
+  test "no overlaps" do
+    assert Scheduler.get_first_overlap([[20, 60], [150, 200]], [[58, 80], [100, 125]], 5) == nil
   end
 end
